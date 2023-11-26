a565b7aa54171e013e394e1ba20f5a897a71d6c2
adanski <adrianz@op.pl>
2023-11-26 23:58:51 +0100

    Dev 2

--- desktop/build.gradle.kts
+++ desktop/build.gradle.kts
@@ -16,15 +16,14 @@ kotlin {
             dependencies {
                 implementation(compose.desktop.currentOs)
                 implementation(project(":common"))
-                implementation("org.apache.pdfbox:pdfbox:3.0.0-RC1")
-                implementation("org.apache.pdfbox:xmpbox:3.0.0-RC1")
+                implementation("org.apache.pdfbox:pdfbox:3.0.0")
+                implementation("org.apache.pdfbox:xmpbox:3.0.0")
                 implementation("org.slf4j:slf4j-api:2.0.7")
                 implementation("org.slf4j:slf4j-jdk14:2.0.7")
                 implementation("org.yaml:snakeyaml:2.0")
                 implementation("com.opencsv:opencsv:5.7.1")
                 implementation("javax.xml.bind:jaxb-api:2.3.1")
                 compileOnly("com.squareup:kotlinpoet:1.14.2")
-                implementation("ca.gosyer:compose-material-dialogs-datetime:0.9.3")
                 //implementation("com.github.ajalt.clikt:clikt:3.5.2")
                 kapt {
                     annotationProcessors("io.pdfx.common.metadata.processing.AnnotationProcessor")
